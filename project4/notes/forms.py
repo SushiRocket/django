fb72aa679a3891d8e98e44aaaa6473a7ce342d04
SushiRocket <kentamori27@gmail.com>
2024-11-20 19:42:02 +0900

    contact_form必須項目を作成

--- project4/notes/forms.py
+++ project4/notes/forms.py
@@ -8,6 +8,17 @@ class CommentCreateForm(forms.ModelForm):
         fields = ('name','text')
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='お名前',max_length=50)
-    email = forms.EmailField(label='メールアドレス')
+    name = forms.CharField(
+        label='お名前',
+        max_length=50,
+        required=True
+        )
+    campany = forms.CharField(
+        label='会社名',
+        required=False
+    )
+    email = forms.EmailField(
+        label='メールアドレス',
+        required=True
+        )
     message = forms.CharField(widget=forms.Textarea,label='メッセージ')
